b51a5af5fbefe0f65c6e3f971e994d7fdca84a7b
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-12-28 15:19:32 +0900

    회원 로직 세팅 (JPA)
    회원 로직 세팅 (JPA)

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +12,13 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@ToString
 public class Member {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
     private String name;
 
     @Embedded
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    /**
+     * 회원가입 처리
+     * @param member
+     * @return Long
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증 비즈니스 로직.
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 처리
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    /**
+     * 회원 전체 처리
+     * @return List<Member>
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한건 조회
+     * @param memberId
+     * @return Member
+     */
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
